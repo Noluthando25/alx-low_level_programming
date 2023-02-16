0dc486eca1abdf6f68d86e00a269ccb2f78cefd5
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-16 09:40:57 -0800

    Nolu

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#include <stdio.h>
-int main(void){puts("Programming is like building a multilingual puzzle\n"); }
+#inlude < stdio.h >
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
