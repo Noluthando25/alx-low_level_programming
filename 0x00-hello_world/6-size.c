b7bb906a07998f11f0a9f4f66f61f13cbc8f3013
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-16 10:40:11 -0800

    Nolu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-       	int a; long int b; long long int c; char d; float f;
+       int a; long int b; long long int c; char d; float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
