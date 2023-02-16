8019720516854c18b7e49ea6d58d8a0330f0c02b
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-16 10:37:00 -0800

    Nolu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,14 @@
-gcc 6-size.c -m32 -o size32 2> /tmp/32
-gcc 6-size.c -m64 -o size64 2> /tmp/64
+#include <stdio.h>
+
+int main(void)
+{
+       	int a; long int b; long long int c; char d; float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
+}
