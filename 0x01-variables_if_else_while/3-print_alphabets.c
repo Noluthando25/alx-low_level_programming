0905478fe75de3b731b492da83d3a43282496499
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-17 13:03:09 -0800

    Nolu

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,12 @@ int main(void)
 {
 	char letter;
 	{
-		for ("letter 'a',letter 'z',letter++");
-		putchar(letter)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
 
-		for ("letter 'A',letter 'Z',letter++\n");
-		putchar(letter)
-
-		putchar(letter)
+		for (letter = 'A'; letter 'Z'; letter++)
+			putchar(letter);
+		putchar('\n');
 	}
 	return (0);
 }
