3c3349ed252a43ff3d49710de4da5f765c217e25
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-17 08:51:43 -0800

    Nolu

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio>
+#include <time.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0);
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("n is positive\n");
+	}
+	else if (n == 0)
+	{
+	printf("n is 0\n");
+	}
+	else if (n < 0)
+	{
+	printf("n is negative\n");
+	}
+
+	printf("exact value of n is : %d\n", a);
+
+	return (0);
+}
+
