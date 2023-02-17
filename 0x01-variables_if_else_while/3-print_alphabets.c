00c240a2cc1c55fb0a9db0139c9ecb57b36cd1b9
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-17 12:55:02 -0800

    Nolu

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char letter;
+	{
+		for ("letter 'a',letter 'z',letter++");
+		putchar(letter)
+
+		for ("letter 'A',letter 'Z',letter++\n");
+		putchar(letter)
+
+		putchar(letter)
+	}
+	return (0);
+}
+
+
