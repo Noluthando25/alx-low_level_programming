f75124920dfca5300ffc94620409209fe74d414b
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-17 13:57:30 -0800

    Nolu

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if (n != 57)
+		putchar(n);
+		{
+			putchar(',');
+			putchar(' ');
+			putchar('\n');
+		}
+	}
+	return (0);
+}
+
