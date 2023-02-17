f338d64319a1c891844b3093fed2e161414b8608
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-17 13:16:38 -0800

    Nolu

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char letter;
+	{
+		for (letter = 'a'; letter <= 'z'; letter-- 'e'; letter-- 'q')
+			putchar(letter)
+
+				putchar('\n')
+	}
+	return (0)
+}
