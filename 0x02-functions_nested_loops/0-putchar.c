9385ee277857eaf01c8ded40f7157db70e5c6fdb
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-21 08:11:03 -0800

    Nolu

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,11 @@
 
 /**
  * main - entry point
- * description - print alphabets in lowercase using putchar
- * print_alphabet - print all in lowecase
+ * description - print alphabets in lowercase using _putchar
+ * print_alphabet - print _putchar all in lowecase
  * Return: 0
  */
-int _putchar(void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
