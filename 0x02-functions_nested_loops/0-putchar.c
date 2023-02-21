51f354b2ea4fed8353c73d56e558aa1c79fb332c
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-21 07:54:55 -0800

    Nolu

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,17 @@
  * print_alphabet - print all in lowecase
  * Return: 0
  */
-int _putchar(void)
+int main(void)
 {
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+
 	return (0);
 
 }
