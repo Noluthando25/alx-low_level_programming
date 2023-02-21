d83a0c78dfbb66f6c5b6fb810f399e1dc1c8ca96
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-21 15:40:15 -0800

    Nolu

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabets
+ * @c: character to br checked
+ * Return: 1 if itd a character, 0 if not
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
