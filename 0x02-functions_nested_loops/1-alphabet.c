5e524ca9a43cbc7d7ed355bfebc3d8be89be7e40
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-21 13:51:18 -0800

    Nolu

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,16 +7,13 @@
  *
  */
 
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
+void print_alphabet(void)
+{	
 char letter;
+
 for (letter = 'a'; letter <= 'z'; letter++)
-{
 	 putchar(letter);
-}
+
 putchar('\n');
 return (0);
 }
