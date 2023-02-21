937b692faa4abc84a43a90855b6e8411112ab16b
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-21 15:05:59 -0800

    Nolu

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print ten times alphabet - in lowercase
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char la;
+	int ten;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(letter);
+
+	for (ten = 0; ten <= 9; ten++)
+
+		putchar('\n');
+
+	return (0);
+}
