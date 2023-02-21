2462108b64d733c9b0e6a4175cb550205d56d49d
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-21 08:29:33 -0800

    Nolu

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ * description - print alphabets in ascending order
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
