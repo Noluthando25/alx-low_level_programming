1daab3d790ff07cf3488b47668cf24b7f78c2179
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-22 04:58:11 -0800

    Nolu

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,12 +20,12 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-	prod = num * mult
+	prod = num * mult;
 		if (prod <= 9)
 			_putchar(' ');
-		else
+		if else
 			_putchar((prod / 10) + '0');
-			_putchar((prod % 10) = '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
