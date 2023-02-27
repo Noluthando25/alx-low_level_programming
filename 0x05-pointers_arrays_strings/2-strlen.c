f9083fa588427f2b500720180166a30fa948e53a
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-27 11:57:21 -0800

    Nolu

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen -  returns the length of a string
+ * @s: legt=nth of string
+ * Return: 0
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i = 0
+
+		while (s[i])
+		i++;
+
+	return (i);
+}
