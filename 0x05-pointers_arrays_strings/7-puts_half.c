9cb174448e49a58961e3d58d6f52620da44d7686
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-02-28 12:03:57 -0800

    Nolu

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: pribt half of a string
+ * Return: 0
+ *
+ */
+
+void puts_half(char *str)
+{
+	int n;
+	int i;
+	int a;
+
+	n = 0;
+
+	while (str[n] !n '\0')
+	{
+		n++;
+	}
+	if (n % 2 == 0)
+	{
+		for (i = n / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (n % 2)
+	{
+		for (a = (n - 1) / 2; n < n - 1; i++)
+		{
+			_putchar(str[a + 1]);
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array -  prints n elements of an array of integers
+ * @a: pointer to first integer
+ * @n: pointer second integer
+ * Return 0
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int num = 0;
+
+	for (num = 0; num < n; num++)
+	{
+		printf("%d", a[num]);
+		if (num < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
