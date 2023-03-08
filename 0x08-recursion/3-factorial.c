05f12dc6540fe75d2258d1aaa2a050a554020359
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-03-08 04:12:14 -0800

    Nolu

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,5 +14,5 @@ int factorial(int n)
 
 	else if (n >= 0 && n <= 1)
 		return (1);
-	return (0);
+	return (1);
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,5 +14,5 @@ int _pow_recursion(int x, int y)
 
 	else if (y == 0)
 		return (1);
-	return (0);
+	return (x);
 }
