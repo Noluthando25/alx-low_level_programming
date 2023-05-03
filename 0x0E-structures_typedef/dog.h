f5ac306c0c90a467c354fd63833398c4d5b87afa
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-05-03 04:18:17 -0700

    Nolu

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,10 @@
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
