ecad336f3215f50156ddf76ef1483ca1df71832e
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-05-03 14:32:39 -0700

    Nolu

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ int count_word(char *s)
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[c] == '')
+		if (s[c] == ' ')
 			a = 0;
 		else if (a == 0)
 		{
@@ -49,7 +49,7 @@ char **strtow(char *str)
 
 			for (i = 0; i <= n; i++)
 			{
-				if (str[i] == '' || str[i] == '\0')
+				if (str[i] == ' ' || str[i] == '\0')
 				{
 					if (c)
 					{
