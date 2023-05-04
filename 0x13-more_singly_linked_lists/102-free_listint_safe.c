281575597b480ee8a75e1ce1440c947a8863a230
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-05-04 10:25:44 -0700

    Nolu

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,90 +1,39 @@
 #include "lists.h"
 
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-
 /**
- * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ * Return: number of elements in the freed list
  */
-size_t looped_listint_len(listint_t *head)
-{
-       	listint_t *tortoise, *hare;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tortoise = head->next;
-	hare = (head->next)->next;
-
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-
-			return (nodes);
-		}
-
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
-
-	return (0);
-}
-
-/**
- * free_listint_safe - Prints a listint_t list safely.
- * @h: A pointer to the head of the listint_t list.
- * Return: The number of nodes in the list.
- */
-
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nodes, index = 0;
-	listint_t *cmp
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-	nodes = looped_listint_count(*h);
+	if (!h || !*h)
+		return (0);
 
-	if (nodes == 0)
+	while (*h)
 	{
-		for (; h != NULL && *h != NULL; nodes++)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			cmp = (*h)->next;
+			temp = (*h)->next;
 			free(*h);
-			*h = cmp;
+			*h = temp;
+			len++;
 		}
-	}
-
-	else
-	{
-		for (index = 0; index < nodes; index++)
+		else
 		{
-			cmp = (*h)->next;
-                        free(*h);
-                        *h = cmp;
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
 		}
-
-		*h = NULL;
 	}
-	h = NULL;
 
-	return (nodes);
+	*h = NULL;
+
+	return (len);
 }
