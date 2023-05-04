edec65d8abd5798c24f250ef8712f5a9be02e49e
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-05-04 09:34:00 -0700

    Nolu

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,14 +8,13 @@ size_t print_listint_safe(const listint_t *head);
  * looped_listint_len - Counts the number of unique nodes
  * in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
- *
  * Return: If the list is not looped - 0.
  * Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *m, *hare;
-	size_t na = 1;
+	size_t a = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
@@ -55,7 +54,6 @@ size_t looped_listint_len(const listint_t *head)
 /**
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
- *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
