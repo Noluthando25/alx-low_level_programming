ccac27b67df13d2517b479208958e83d63e292f7
Noluthando25 <nonhlanhlafumba@gmail.com>
2023-05-03 17:00:52 -0700

    Nolu

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (!ptr)
 		return (malloc(new_size));
-	ptr1 = malloc(new_size);
+	a = malloc(new_size);
 	if (!a)
 		return (NULL);
 	ptr2 = ptr;
