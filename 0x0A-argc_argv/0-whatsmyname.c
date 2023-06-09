d31b373f8b54c4fbc4ba0b82be2d76a7ae1f6f83
Noluthando25 <Nonhlanhlafumba@gamil.com>
2023-06-09 11:47:50 -0700

    Nolu

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argv: second argument
+ * @argc: first argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
