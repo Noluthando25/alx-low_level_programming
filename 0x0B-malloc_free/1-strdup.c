317d4ce6171072f8164fb29fd1d60fe5839ddcc5
Noluthando25 <Nonhlanhlafumba@gamil.com>
2023-06-13 04:36:31 -0700

    Nolu

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strdup - a pointer to a new string which is a duplicate of the string
+ * @str: string
+ * Return: NULL if insufficient memory was available
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate = NULL;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	duplicate = malloc(strlen(str) + 1);
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(duplicate, str);
+	return (duplicate);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * str_concat -  concatenates two strings
+ * @s1: pointer
+ * @s2: string
+ * Return: NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concatenated = NULL;
+	int s1_length = 0, s2_length = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	s1_length = strlen(s1);
+	s2_length = strlen(s2);
+	concatenated = malloc(sizeof(char) * (s1_length + s2_length + 1));
+	if (concatenated == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(concatenated, s1);
+	strcat(concatenated, s2);
+	return (concatenated);
+}
