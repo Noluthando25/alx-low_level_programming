8a03b05b0bbe7b44c75407d8eea418d15cf1fbfa
Noluthando25 <Nonhlanhlafumba@gamil.com>
2023-06-15 04:10:34 -0700

    Nolu

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: argument
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+ * string_nconcat - that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: 
