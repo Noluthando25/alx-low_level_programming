4b5f9205f9b73483f0f68fd21fd8e15a3951e1b1
Noluthando25 <Nonhlanhlafumba@gamil.com>
2023-06-20 07:37:02 -0700

    Nolu

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,4 +7,27 @@
  * string_nconcat - that concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: 
+ * @n: integer
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	if
+		(s1 == NULL) s1 = "";
+	if
+		(s2 == NULL) s2 = "";
+	size_t s1_len = strlen(s1);
+	size_t s2_len = strlen(s2);
+
+	if
+		(n >= s2_len) n = s2_len;
+	char *result = malloc(s1_len + n + 1);
+
+	if
+		(result == NULL) return NULL;
+	memcpy(result, s1, s1_len);
+	memcpy(result + s1_len, s2, n);
+	result[s1_len + n] = '\0';
+	return (result);
+}
