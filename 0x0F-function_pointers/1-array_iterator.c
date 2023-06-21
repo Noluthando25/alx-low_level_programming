8c5dfa381522b38d7801e5609bb0800d91e7f83a
Noluthando25 <Nonhlanhlafumba@gamil.com>
2023-06-21 05:02:32 -0700

    Nolu

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,18 +14,26 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 	size_t s1_len = strlen(s1);
 	size_t s2_len = strlen(s2);
 
 	if (n >= s2_len)
+	{
 		n = s2_len;
+	}
 	char *result = malloc(s1_len + n + 1);
 
 	if (result == NULL)
+	{
 		return (NULL);
+	}
 	memcpy(result, s1, s1_len);
 	memcpy(result + s1_len, s2, n);
 	result[s1_len + n] = '\0';
